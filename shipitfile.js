0b5e6331d0bac0d187d21a24073a204209a6fb17
mario <mario.fredrick@mncgroup.com>
2018-09-12 21:53:30 +0700

    shipit deploy menggunakan file .env untuk menentukan path ssh private key Cara jalanin deploy menggunakan perintah `npx shipit staging deployCore`

--- shipitfile.js
+++ shipitfile.js
@@ -1,3 +1,7 @@
+
+require("dotenv").config();
+
+
 // shipitfile.js
 module.exports = shipit => {
   // Load shipit-deploy tasks
@@ -7,16 +11,27 @@ module.exports = shipit => {
     default: {
       deployTo: "/home/mario/core-app",
       repositoryUrl: "https://mnc-repo.mncdigital.com/big-data/core-uam.git",
-      ignores: ["node_modules", ".git"]
+      ignores: ["node_modules", ".git", "shipitfile.js"],
+      key: process.env.SSH_PRIV_KEY_PATH
     },
     staging: {
       servers: "mario@10.10.16.152"
+      // key: '/mnt/c/Users/mario.fredrick/.ssh/id_rsa.pub'
+      // workspace: '/home/mario/core-app/current'
     }
   })
 
   shipit.task("deployCore", async () => {
-    shipit.remote("ls /home/mario/core-app")
-    // await shipit.copyToRemote("*", "/home/mario/core-app/")
+    // shipit.remote("cd /home/mario/core-app/current")
+    // shipit.remote("ls /home/mario/core-app/current")
+    await shipit.remote("cd /home/mario/core-app/current && rm -R ./*")
+    await shipit.copyToRemote("*", "/home/mario/core-app/current")
+
+    shipit.remote("cd /home/mario/core-app/current && npm install").then( () => {
+      // shipit.remote("npm update")
+      // shipit.remote("ls -al")
+    })
   })
-  shipit.start("deployCore")
+
+  // shipit.start("deployCore")
 }
